b0927a878e2e99a46aa15d234607963d3546ed0d
rydgecraker <rcraker5@student.cvtc.edu>
2018-04-11 18:29:56 -0500

    possibly made queries work

--- webservice.php
+++ webservice.php
@@ -2,25 +2,76 @@
     ini_set('display_errors', 1); 
     error_reporting(-1);
         /* require the user as the parameter */
-    if(isset($_GET['user']) && intval($_GET['user'])) {
+    if(isset($_GET['username']) && intval($_GET['username'])) {
+        $username = intval($_GET['username']);
+        $host = 'localhost';
+        $db   = 'StillAlive';
+        $user = 'root';
+        $pass = 'stillalive';
+        $charset = 'utf8mb4';
 
-                    /*** mysql hostname ***/
-         $hostname = 'localhost';
-
-         /*** mysql username ***/
-         $username = 'root';
-
-         /*** mysql password ***/
-         $password = 'stillalive';
-
-         try {
-             $dbh = new PDO("mysql:host=$hostname;dbname=StillAlive", $username, $password);
-             /*** echo a message saying we have connected ***/
-             echo 'Connected to database';
-             }
-         catch(PDOException $e)
-             {
-             echo $e->getMessage();
-             }
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+        
+        $query = $pdo->prepare('SELECT * FROM Players WHERE playerID = ?');
+        $query->execute([$username]);
+        while ($row = $query->fetch())
+        {
+            echo $row['name'] . "\n";
+        }
+        
+        
+                    /* soak in the passed variable or set our own */
+//            $number_of_posts = isset($_GET['num']) ? intval($_GET['num']) : 10; //10 is the default
+//            $format = strtolower($_GET['format']) == 'json' ? 'json' : 'xml'; //xml is the default
+//            $user_id = intval($_GET['user']); //no default
+//
+//            /* connect to the db */
+//            $link = mysql_connect('localhost/phpmyadmin','root','stillalive', "StillAlive") or die('Cannot connect to the DB');
+//
+//            /* grab the posts from the db */
+//            $query = "SELECT * FROM Players WHERE playerID = $user_id";
+//            $result = mysql_query($query,$link) or die('Errant query:  '.$query);
+//
+//            /* create one master array of the records */
+//            $posts = array();
+//            if(mysql_num_rows($result)) {
+//                    while($post = mysql_fetch_assoc($result)) {
+//                            $posts[] = array('post'=>$post);
+//                    }
+//            }
+//
+//            /* output in necessary format */
+//            if($format === 'json') {
+//                    header('Content-type: application/json');
+//                    echo json_encode(array('posts'=>$posts));
+//            }
+//            else {
+//                    header('Content-type: text/xml');
+//                    echo '<posts>';
+//                    foreach($posts as $index => $post) {
+//                            if(is_array($post)) {
+//                                    foreach($post as $key => $value) {
+//                                            echo '<',$key,'>';
+//                                            if(is_array($value)) {
+//                                                    foreach($value as $tag => $val) {
+//                                                            echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+//                                                    }
+//                                            }
+//                                            echo '</',$key,'>';
+//                                    }
+//                            }
+//                    }
+//                    echo '</posts>';
+//            }
+//
+//            /* disconnect from the db */
+//            mysql_close($link);
+            
     }
 ?> 
